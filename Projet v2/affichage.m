1533f8fea8114cdb267dbdc82b85ff966beb3339
Uralth <58824882+Uralth@users.noreply.github.com>
2021-01-23 23:34:16 +0100

    pour visu matlab

---
+++ Projet v2/affichage.m
@@ -0,0 +1,29 @@
+clear all
+close all
+
+%% Ouverture des fichiers
+fileU = fopen('Untitled.txt','r');
+fileV = fopen('Untitled 2.txt','r');
+img = imread('untitled.bmp');
+
+%Lecture des données
+A = fscanf(fileU,"%f");
+A1 = fscanf(fileV,"%f");
+
+%Dimensions de la matrice
+dim = A(1:2).';
+
+%Capture des données d'images (2 premieres valeurs = dimensions)
+B = A(3:end);
+B1 = A1(3:end);
+
+%Formattage aux dimensions appropriees
+B = reshape(B, dim);
+B1 = reshape(B1, dim);
+
+%Affichage de l'image
+image(img)
+hold on
+
+%Traçage des vecteurs
+quiver(B,B1);
--- Projet v2/main.c
+++ Projet v2/main.c
@@ -5,8 +5,12 @@
 #include "portfolio.h"
 #define FILENAME "Untitled.bmp"
 #define FILENAME2 "Untitled_2.bmp"
+#define FILENAME3 "Untitled.txt"
+#define FILENAME4 "Untitled 2.txt"
 
 int main(){
+    FILE *fichierTxt;
+    FILE *fichierTxt2;
     BmpImg image = readBmpImage(FILENAME);
     BmpImg image2= readBmpImage(FILENAME2);
     double** matrice = bmpToMatrice(image);
@@ -26,7 +30,8 @@ int main(){
     fillGradY(matrice,matriceGradY,image.dimX,image.dimY);
     fillGrad_t(matrice,matrice2,matriceGrad_t,image.dimX,image.dimY);
 
-
+    fichierTxt = fopen(FILENAME3, "w");
+    fichierTxt2 = fopen(FILENAME4, "w");
 
     /* Calcul de u */
     vit1 = vit(matriceGradX,matriceGradY,matriceGrad_t,image.dimX,image.dimY,3.0);
@@ -41,6 +46,18 @@ int main(){
     //printf("=================test=============\n");
     //printf("moyenne = %f",calcMoyCase(matriceGradX, image.dimX, image.dimY));
     //printMatrice(u_m.U_moy,image.dimX,image.dimY);
+    if( fichierTxt != NULL ) {
+        MatriceToTxt(vit1.u, image.dimX, image.dimY,fichierTxt);
+    } else { // Sinon on affiche un message
+        printf ( " Erreur d'ouverture du fichier !" );
+    }
+
+    if( fichierTxt2 != NULL ) {
+        MatriceToTxt(vit1.v, image.dimX, image.dimY,fichierTxt2);
+    } else { // Sinon on affiche un message
+        printf ( " Erreur d'ouverture du fichier !" );
+    }
+
     freeBmpImg(&image);
     freeBmpImg(&image2);
     return 0;
